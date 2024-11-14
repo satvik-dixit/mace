81a81f596dfa7aa9cd7f25a659cf205b54e39b16
Satvik Dixit <78974280+satvik-dixit@users.noreply.github.com>
2024-11-14 14:03:58 -0500

    Update evaluator.py

--- mace/evaluator.py
+++ mace/evaluator.py
@@ -1,17 +1,19 @@
-import os
-import re
-import json
+
 import numpy as np
 import torch
+import os
+from msclap import CLAP
+from torchaudio import load, save
+import torch.nn.functional as F
+import tempfile
 from tqdm import trange
 from .model import BERTFlatClassifier
 from .data import infer_preprocess
-from functools import lru_cache
-# from sentence_transformers import SentenceTransformer
 from transformers import AutoTokenizer
 from transformers import logging as trf_logging
 import transformers
-from msclap import CLAP
+
+clap_model = CLAP(version='2023', use_cuda=True)  
 
 PRETRAIN_ECHECKERS = {
     'echecker_clotho_audiocaps_base': ("https://github.com/blmoistawinde/fense/releases/download/V0.1/echecker_clotho_audiocaps_base.ckpt", "1a719f090af70614bbdb9f9437530b7e133c48cfa4a58d964de0d47fc974a2fa"),
@@ -50,7 +52,6 @@ def load_pretrain_echecker(echecker_model, device='cuda', use_proxy=False, proxi
     clf.to(device)
     return clf
 
-
 class Evaluator:
     def __init__(self, batch_size=32, device='cuda', clap_model="MS-CLAP", echecker_model="echecker_clotho_audiocaps_base", error_threshold=0.97, penalty=0.3, use_proxy=False, proxies=None):
         # assert sbert_model in {'paraphrase-MiniLM-L6-v2', 'paraphrase-TinyBERT-L6-v2', 'paraphrase-mpnet-base-v2'}
@@ -70,24 +71,6 @@ class Evaluator:
             self.echecker_tokenizer = AutoTokenizer.from_pretrained(self.echecker.model_type)
             self.echecker.to(device)
             self.echecker.eval() 
-
-    def encode_sents_clap(self, sents, batch_size=32):
-        # return self.sbert.encode(sents, convert_to_tensor=True, normalize_embeddings=True, batch_size=batch_size, show_progress_bar=True)
-        # return torch.stack([self.clap_model.get_text_embeddings([sent]).to('cuda') for sent in sents], dim=0).squeeze()
-        return self.clap_model.get_text_embeddings(sents).to('cuda') 
-
-    def encode_audios_clap(self, audio_paths):
-        audio_embs = torch.stack([self.clap_model.get_audio_embeddings([audio_path]).to('cuda') for audio_path in audio_paths], dim=0)
-        return audio_embs
-    
-    @lru_cache(maxsize=32)   # reuse cache if encode the same sentence
-    def encode_sent_clap(self, sent):
-        # return self.sbert.encode(sent, convert_to_tensor=True, normalize_embeddings=True)
-        return self.clap_model.get_text_embeddings(sent).to('cuda') 
-    
-    def encode_audio_clap(self, audio_path):
-        # audio_embs = torch.stack([self.clap_model.get_audio_embeddings([audio_file]).to('cuda') for audio_file in audio_files], dim=0)
-        return self.clap_model.get_audio_embeddings([audio_path]).to('cuda')
 
 
     def detect_error_sents(self, sents, batch_size=32):
@@ -111,7 +94,7 @@ class Evaluator:
             probs = np.concatenate(probs)
         return (probs > self.error_threshold).astype(float)
 
-    @lru_cache(maxsize=32)   # reuse cache if infer with the same sentence
+    # @lru_cache(maxsize=32)   # reuse cache if infer with the same sentence
     def detect_error_sent(self, sent, return_error_prob=False):
         batch = infer_preprocess(self.echecker_tokenizer, [sent], max_len=64)
         for k, v in batch.items():
@@ -125,45 +108,102 @@ class Evaluator:
             return has_error, probs[0, -1]
         else:
             return has_error 
+    
+
+    def get_chunked_audio_embedding(self, audio_path, chunk_duration=7.0, sample_rate=16000):
+        waveform, sr = load(audio_path)  # Load the audio file
+        sample_rate = sr
+
+        chunk_samples = int(chunk_duration * sample_rate)  # Number of samples in each 7-second chunk
+        num_chunks = (waveform.size(1) + chunk_samples - 1) // chunk_samples  # Calculate total number of chunks
+
+        # Calculate the embedding for each chunk
+        chunk_embeddings = []
+        chunk_durations = []
+        for i in range(num_chunks):
+            start = i * chunk_samples
+            end = min(start + chunk_samples, waveform.size(1))
+            chunk = waveform[:, start:end]
+            chunk_duration_seconds = chunk.size(1) / sample_rate  # Calculate the actual duration of this chunk
+            chunk_durations.append(chunk_duration_seconds)
+            
+            # Create a temporary file for the chunk
+            with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as temp_file:
+                temp_path = temp_file.name
+                save(temp_path, chunk, sample_rate=sample_rate)  
+                chunk_embedding = clap_model.get_audio_embeddings([temp_path]).to('cuda')
+                chunk_embeddings.append(chunk_embedding)
+                os.remove(temp_path)
+
+        # Stack embeddings and calculate weighted mean
+        chunk_embeddings = torch.stack(chunk_embeddings, dim=0).squeeze()
+        chunk_durations = torch.tensor(chunk_durations, device=chunk_embeddings.device)  # Convert durations to tensor
+        total_duration = chunk_durations.sum()
+        weights = (chunk_durations / total_duration).view(-1, 1)  # Normalize durations to get weights, shape [num_chunks, 1]
+
+        # Compute the weighted mean of embeddings
+        weighted_mean_embedding = torch.sum(chunk_embeddings * weights, dim=0).unsqueeze(0) 
+        print('weighted_mean_embedding shape:', weighted_mean_embedding.shape)
+        return weighted_mean_embedding
+
+    def get_similarity_score(self, all_preds_text, all_refs_text=None, audio_files=None, method='text-text', average=True):
+
+        N = len(all_preds_text)
+        all_preds_text = np.array(all_preds_text, dtype=str)
+        print('all_preds_text shape:', len(all_preds_text))
+
+        if method=='text-text':
+            K = len(all_refs_text[0]) if all_refs_text is not None else 1
+            all_refs_text = np.array(all_refs_text, dtype=str)
+            print('all_refs_text shape:', len(all_refs_text))
+            print('all_refs_text shape:', len(all_refs_text[0]))
+        elif method=='audio-text':
+            K = 1
+
+        score = torch.zeros((N, K))
+
+        # For text-text
+        if method == 'text-text':
+            preds_clap = torch.stack([clap_model.get_text_embeddings([pred]).to('cuda') for pred in all_preds_text], dim=0).squeeze()
+            print('preds_clap shape:', preds_clap.shape)
+            refs_clap = torch.stack([clap_model.get_text_embeddings(refs).to('cuda') for refs in all_refs_text], dim=0)
+            print('refs_clap shape:', refs_clap.shape)
+            for i in range(K):
+                score[:, i] = torch.Tensor([cosine_similarity(input, target) for input, target in zip(preds_clap, refs_clap[:, i])])
+            print('score:', score)
+
+        # For audio-text
+        elif method == 'audio-text':
+            if audio_files is None:
+                raise ValueError("Audio files must be provided for ms_clap_audio_caption.")
+            preds_clap = torch.stack([clap_model.get_text_embeddings([pred]).to('cuda') for pred in all_preds_text], dim=0).squeeze()
+            print('preds_clap shape:', preds_clap.shape)
+            print('audio_files:', audio_files)
+            audio_embs = torch.stack([self.get_chunked_audio_embedding(audio_file) for audio_file in audio_files], dim=0)
+            print('audio_embs shape:', audio_embs.shape)
+            for i in range(1):
+                score[:, i] = torch.Tensor([cosine_similarity(input, target) for input, target in zip(preds_clap, audio_embs[:, i])])
+            print('score:', score)
+            print('score shape:', score.shape)
+
+        elif method=='combined':
+            text_score = self.get_similarity_score(all_preds_text, all_refs_text, method='text-text', average=True, audio_files=None)
+            audio_score = self.get_similarity_score(all_preds_text, all_refs_text, method='audio-text', average=True, audio_files=audio_files)
+            score = (text_score + audio_score) / 2
+            return score
+
+        # Calculate average or max score
+        # score = score.mean(dim=1) if average else score.max(dim=1)[0]
+        # print('avg_score:', score)
+        print('avg_score shape:', score.shape)
+        return score    
+
 
     def corpus_score(self, cands, list_refs=None, audio_paths=None, config='text-text', agg_score='mean'):
         assert config in ['combined', 'audio-text', 'text-text']
         assert agg_score in {'none', 'mean', 'max'}
 
-        if config=='text-text':
-            assert len(cands)==len(list_refs)
-            rng_ids = [0]
-            all_refs = []
-            for lst in list_refs:
-                rng_ids.append(rng_ids[-1]+len(lst))
-                all_refs.extend(lst)
-            emb_cands = self.encode_sents_clap(cands, self.batch_size)
-            emb_refs = self.encode_sents_clap(all_refs, self.batch_size)
-            # sim_scores = [(emb_cands[i] @ emb_refs[rng_ids[i]:rng_ids[i+1]].T).mean().detach().cpu().item() for i in range(len(cands))]
-            sim_scores = [cosine_similarity(emb_cands[i], emb_refs[rng_ids[i]:rng_ids[i+1]]).mean().detach().cpu().item() for i in range(len(cands))]
-        elif config=='audio-text':
-            assert len(cands)==len(audio_paths)
-            emb_cands = self.encode_sents_clap(cands, self.batch_size)
-            emb_audios = self.encode_audios_clap(audio_paths)
-            # sim_scores = [(emb_cands[i] @ emb_audios[i].T).mean().detach().cpu().item() for i in range(len(cands))]
-            sim_scores = [cosine_similarity(emb_cands[i], emb_audios[i]).mean().detach().cpu().item() for i in range(len(cands))]
-        elif config=='combined':
-            assert len(cands)==len(list_refs)
-            assert len(cands)==len(audio_paths)
-            rng_ids = [0]
-            all_refs = []
-            for lst in list_refs:
-                rng_ids.append(rng_ids[-1]+len(lst))
-                all_refs.extend(lst)
-            emb_cands = self.encode_sents_clap(cands, self.batch_size)
-            emb_refs = self.encode_sents_clap(all_refs, self.batch_size)
-            # text_sim_scores = [(emb_cands[i] @ emb_refs[rng_ids[i]:rng_ids[i+1]].T).mean().detach().cpu().item() for i in range(len(cands))]
-            text_sim_scores = [cosine_similarity(emb_cands[i], emb_refs[rng_ids[i]:rng_ids[i+1]]).mean().detach().cpu().item() for i in range(len(cands))]
-
-            emb_audios = self.encode_audios_clap(audio_paths)
-            # audio_sim_scores = [(emb_cands[i] @ emb_audios[i].T).mean().detach().cpu().item() for i in range(len(cands))]
-            audio_sim_scores = [cosine_similarity(emb_cands[i], emb_audios[i]).mean().detach().cpu().item() for i in range(len(cands))]
-            sim_scores = (text_sim_scores + audio_sim_scores) / 2
+        sim_scores = self.get_similarity_score(cands, list_refs, audio_paths, config, average=True)
 
         if self.echecker_model == "none":
             if agg_score == 'mean':
@@ -183,39 +223,6 @@ class Evaluator:
                 return np.max(penalized_scores)
             else:
                 return penalized_scores
-
-    def sentence_score(self, cand, refs=None, audio_path=None, config='text-text', return_error_prob=False):
-        assert config in ['combined', 'audio-text', 'text-text']
-        
-        emb_cand = self.encode_sent_clap(cand)
-        if config=='audio-text':
-            emb_refs_audio = self.encode_audio_clap(audio_path)
-            # scores = emb_cand @ emb_refs_audio.T
-            scores = cosine_similarity(emb_cand, emb_refs_audio)
-
-        elif config=='text-text':
-            emb_refs_text = self.encode_sents_clap(refs, self.batch_size)
-            # scores = emb_cand @ emb_refs_text.T
-            scores = cosine_similarity(emb_cand, emb_refs_text)
-
-        elif config=='combined':
-            emb_refs_audio = self.encode_audio_clap(audio_path)
-            emb_refs_text = self.encode_sents_clap(refs, self.batch_size)
-            # scores = emb_cand @ emb_refs_audio.T + emb_cand @ emb_refs_text.T
-            scores = (cosine_similarity(emb_cand, emb_refs_audio) + cosine_similarity(emb_cand, emb_refs_text)) / 2
-        
-        if self.echecker_model == "none":
-            return scores.mean().detach().cpu().item()
-        else:
-            score = scores.mean().detach().cpu().item()
-            if not return_error_prob:
-                has_error = self.detect_error_sent(cand)
-                penalized_score = (1-self.penalty)*score if has_error else score
-                return penalized_score
-            else:
-                has_error, error_prob = self.detect_error_sent(cand, return_error_prob)
-                penalized_score = (1-self.penalty)*score if has_error else score
-                return score, error_prob, penalized_score
 
 if __name__ == "__main__":
     evaluator = Evaluator(device='cuda', clap_model='MS-CLAP', echecker_model='echecker_clotho_audiocaps_tiny')
