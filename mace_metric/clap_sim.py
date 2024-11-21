800cfb6bbcd9c9eb489b5e71e4639271273ce866
Satvik Dixit <78974280+satvik-dixit@users.noreply.github.com>
2024-11-21 18:40:57 -0500

    Update clap_sim.py

--- mace_metric/clap_sim.py
+++ mace_metric/clap_sim.py
@@ -19,6 +19,7 @@ pylog = logging.getLogger(__name__)
 DEFAULT_CLAP_SIM_MODEL = "MS-CLAP-2023"
 
 def clap_sim(
+    method: str,
     candidates: list[str],
     mult_references: list[list[str]],
     return_all_scores: bool = True,
@@ -29,7 +30,7 @@ def clap_sim(
     verbose: int = 0,
 ) -> Union[Tensor, tuple[dict[str, Tensor], dict[str, Tensor]]]:
     """Cosine-similarity of the CLAP embeddings.
-
+    :param method: The method used to encode the sentences. Can be "text" or "audio".
     :param candidates: The list of sentences to evaluate.
     :param mult_references: The list of list of sentences used as target.
     :param return_all_scores: If True, returns a tuple containing the globals and locals scores.
@@ -51,16 +52,19 @@ def clap_sim(
     rng_ids = [0]
     for refs in mult_references:
         rng_ids.append(rng_ids[-1] + len(refs))
+
     flat_references = [ref for refs in mult_references for ref in refs]
 
     cands_embs = _encode_sents_clap(clap_model, candidates, batch_size, verbose)
-    mrefs_embs = _encode_sents_clap(clap_model, flat_references, batch_size, verbose)
+    if method == 'text':
+        mrefs_embs = _encode_sents_clap(clap_model, flat_references, batch_size, verbose)
+    elif method == 'audio':
+        mrefs_embs = _encode_audios_clap(clap_model, flat_references, batch_size, verbose)
+    else:
+        raise ValueError(f"Invalid method: {method}")
 
     # Compute CLAP similarities
-    clap_sim_scores = [
-        (cands_embs[i] @ mrefs_embs[rng_ids[i] : rng_ids[i + 1]].T).mean().item()
-        for i in range(len(cands_embs))
-    ]
+    clap_sim_scores = [(cands_embs[i] @ mrefs_embs[rng_ids[i] : rng_ids[i + 1]].T).mean().item()for i in range(len(cands_embs))]
     clap_sim_scores = np.array(clap_sim_scores)
 
     # Aggregate and return
@@ -83,14 +87,14 @@ def clap_sim(
 
 
 def _load_clap(
-    sbert_model: Union[str, CLAP] = DEFAULT_CLAP_SIM_MODEL,
+    clap_model: Union[str, CLAP] = DEFAULT_CLAP_SIM_MODEL,
     device: Union[str, torch.device, None] = "cuda_if_available",
     reset_state: bool = True,
 ) -> CLAP:
     state = torch.random.get_rng_state()
 
     device = _get_device(device)
-    if isinstance(sbert_model, str):
+    if isinstance(clap_model, str):
         clap_model = CLAP(version='2023', use_cuda=(device=='cuda'))
 
     clap_model.to(device=device)
@@ -109,6 +113,21 @@ def _encode_sents_clap(
 ) -> Tensor:
     return clap_model.get_text_embeddings(
         sents,
+        # convert_to_tensor=True,
+        # normalize_embeddings=True,
+        # batch_size=batch_size,
+        # show_progress_bar=verbose >= 2,
+    )  # type: ignore
+
+@torch.no_grad()
+def _encode_audios_clap(
+    clap_model: CLAP,
+    audio_paths: list[str],
+    batch_size: int = 32,
+    verbose: int = 0,
+) -> Tensor:
+    return clap_model.get_audio_embeddings(
+        audio_paths,
         # convert_to_tensor=True,
         # normalize_embeddings=True,
         # batch_size=batch_size,
